dac208bbf688873c20ec232145da257af07db1ab
Ilia Kuznetsov <ilyakuz@amazon.com>
2023-07-12 15:33:18 +0100

    refactor: Update k8s deployment to use private reg credentials

--- deploy/create-secrets.sh
+++ deploy/create-secrets.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+kubectl get secret regcred --namespace default -o yaml | kubectl apply --namespace letsdeploy -f -
 kubectl --namespace letsdeploy create secret generic letsdeploy-secrets --from-env-file .env
