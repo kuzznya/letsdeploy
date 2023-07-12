3f5bf8fcb8e72c2ec9db74c75dfe280b4c0c371e
Ilia Kuznetsov <ilyakuz@amazon.com>
2023-07-12 15:37:30 +0100

    fix: Update secrets script to remove namespace during copy
    skip ci

--- deploy/create-secrets.sh
+++ deploy/create-secrets.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-kubectl get secret regcred --namespace default -o yaml | kubectl apply --namespace letsdeploy -f -
+set -e
+
+kubectl get secret regcred --namespace default -o yaml | grep -v '^\s*namespace:\s' | kubectl apply --namespace letsdeploy -f -
 kubectl --namespace letsdeploy create secret generic letsdeploy-secrets --from-env-file .env
