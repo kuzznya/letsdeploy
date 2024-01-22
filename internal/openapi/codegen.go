db128e63f8350d8112213f21b5cd1443c9433f9a
Ilia Kuznetsov <ikuz2002@gmail.com>
2024-01-22 01:29:03 +0000

    fix: Upgrade oapi-codegen

--- internal/openapi/codegen.go
+++ internal/openapi/codegen.go
@@ -1,4 +1,6 @@
 package openapi
 
-//go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen -config oapi-codegen-models.yaml ../../api/openapi.yaml
-//go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen -config oapi-codegen-server.yaml ../../api/openapi.yaml
+import _ "github.com/deepmap/oapi-codegen/v2/pkg/codegen"
+
+//go:generate go run github.com/deepmap/oapi-codegen/v2/cmd/oapi-codegen -config oapi-codegen-models.yaml ../../api/openapi.yaml
+//go:generate go run github.com/deepmap/oapi-codegen/v2/cmd/oapi-codegen -config oapi-codegen-server.yaml ../../api/openapi.yaml
