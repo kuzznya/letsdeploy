f5f08ec94f5e81859994df1ac25d0bb7099776dc
Ilia Kuznetsov <ikuz2002@gmail.com>
2024-03-13 21:52:44 +0000

    refactor: Migrate to bootstrap-vue-next

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,10 +2,19 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+// @ts-ignore
+import Components from "unplugin-vue-components/vite";
+// @ts-ignore
+import { BootstrapVueNextResolver } from "unplugin-vue-components/resolvers";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [BootstrapVueNextResolver()],
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
